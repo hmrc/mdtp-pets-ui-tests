497585b02e403aa1d268e41908669436ef6220fa
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2025-01-20 10:54:40 +0000

    PLATUI-3434 refactor prototype startup script

--- start_prototype.sh
+++ start_prototype.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+# This file is not a recommended approach to testing
+# Please be aware that the contets of this script is not a solution to testing your prototypes
+
 TEMP_FOLDER="tmp-test-script"
-BACK_TWO_FOLDERS="../../"
 REPO="example-ui-journey-tests-prototype"
-BRANCH=$1
-BROWSER=$2
-ENVIRONMENT=$3
 
 mkdir $TEMP_FOLDER
 cd $TEMP_FOLDER
@@ -13,7 +12,3 @@ git clone https://github.com/hmrc/$REPO
 cd $REPO
 npm install
 echo y | npm run dev > /dev/null 2>&1 & # The ampersand (&) is needed to run this task in the background and the echo y is to skip through an analytics question
-cd $BACK_TWO_FOLDERS
-git checkout ${BRANCH:=request-a-pet-journey}
-sbt scalafmtSbt
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dsecurity.assessment=false -Dbrowser.option.headless=false test testReport
